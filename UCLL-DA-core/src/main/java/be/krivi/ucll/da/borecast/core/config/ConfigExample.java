9e294ceb8d4bbb30135aeffe4caedd5ae2c396be
Kristof Dewilde <me@krivi.be>
2016-10-02 19:58:09 +0200

    Fixed application config class

---
+++ UCLL-DA-core/src/main/java/be/krivi/ucll/da/borecast/core/config/ConfigExample.java
@@ -0,0 +1,10 @@
+package be.krivi.ucll.da.borecast.core.config;
+
+public class ConfigExample{
+
+    // OpenWeatherMap API key
+    public static final String OWM_API_KEY = "super-secret-key";
+    // OpenWeatherMap API version
+    public static final double OWM_API_VERSION = 2.5;
+
+}
