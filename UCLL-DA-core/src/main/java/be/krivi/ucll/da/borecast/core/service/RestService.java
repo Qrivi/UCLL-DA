ad66e35d1685259267331ed70a9f13a094f36ca3
Kristof Dewilde <me@krivi.be>
2016-10-02 21:06:25 +0200

    Commented out obsolete and Spring dependant code

--- UCLL-DA-core/src/main/java/be/krivi/ucll/da/borecast/core/service/RestService.java
+++ UCLL-DA-core/src/main/java/be/krivi/ucll/da/borecast/core/service/RestService.java
@@ -1,28 +1,25 @@
 package be.krivi.ucll.da.borecast.core.service;
 
-import be.krivi.ucll.da.borecast.core.config.Config;
-import be.krivi.ucll.da.borecast.core.entity.rest.Response;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
-
 /**
  * Created by Krivi on 23/09/2016.
  */
 
 public class RestService{
 
-    private static String OWM_API_URL = "http://api.openweathermap.org/data/{version}/forecast/daily?appid={key}&q={location}";
+    // WE'LL NOT BE USING SPRING TO CONSUME JSON
 
-    public void fetchForecast( String location ){
-
-        RestTemplate template = new RestTemplate();
-
-        try{
-            Response response = template.getForObject( OWM_API_URL, Response.class, Config.OWM_API_VERSION, Config.OWM_API_KEY, location );
-
-            System.out.println( "cool" );
-        }catch( RestClientException e ){
-            System.out.println( "niet cool" );
-        }
-    }
+//    private static String OWM_API_URL = "http://api.openweathermap.org/data/{version}/forecast/daily?appid={key}&q={location}";
+//
+//    public void fetchForecast( String location ){
+//
+//        RestTemplate template = new RestTemplate();
+//
+//        try{
+//            Response response = template.getForObject( OWM_API_URL, Response.class, Config.OWM_API_VERSION, Config.OWM_API_KEY, location );
+//
+//            System.out.println( "cool" );
+//        }catch( RestClientException e ){
+//            System.out.println( "niet cool" );
+//        }
+//    }
 }
