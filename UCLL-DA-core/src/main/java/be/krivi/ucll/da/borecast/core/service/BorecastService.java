7f9e2773098ce4da65df72268d140a2ddcbb2b66
Kristof Dewilde <me@krivi.be>
2016-10-02 21:06:53 +0200

    Refactored service class

---
+++ UCLL-DA-core/src/main/java/be/krivi/ucll/da/borecast/core/service/BorecastService.java
@@ -0,0 +1,62 @@
+package be.krivi.ucll.da.borecast.core.service;
+
+import be.krivi.ucll.da.borecast.core.entity.City;
+import be.krivi.ucll.da.borecast.core.exception.DatabaseException;
+import be.krivi.ucll.da.borecast.core.repository.CityRepository;
+import be.krivi.ucll.da.borecast.core.repository.RepositoryFactory;
+
+import java.util.Collection;
+import java.util.Properties;
+
+/**
+ * Created by Jan on 28/09/2016.
+ */
+
+public class BorecastService{
+
+    private CityRepository cityRepository;
+    
+    public BorecastService( Properties properties ){
+        cityRepository = RepositoryFactory.createCityRepository( properties );
+    }
+
+    public void openConnection(){
+        cityRepository.openConnexion();
+    }
+
+    public void closeConnection(){
+        cityRepository.closeConnexion();
+    }
+
+    //****************************************************************
+    // region City
+    //****************************************************************
+
+    public City getCityById( Integer id ) throws DatabaseException{
+        return cityRepository.getById( id );
+    }
+
+    public City getCityByName( String name ) throws DatabaseException{
+        return cityRepository.getByName( name );
+    }
+
+    public Collection<City> getAllCitys() throws DatabaseException{
+        return cityRepository.getAll();
+    }
+
+    public void addCity( City city ) throws DatabaseException{
+        cityRepository.add( city );
+    }
+
+    public void deleteCity( City city ) throws DatabaseException{
+        cityRepository.delete( city );
+    }
+
+    public void updateCity( City city ) throws DatabaseException{
+        cityRepository.update( city );
+    }
+    
+    //****************************************************************
+    // endregion
+    //****************************************************************
+}
