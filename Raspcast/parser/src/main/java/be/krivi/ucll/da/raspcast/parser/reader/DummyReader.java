79774606588e0e25b38f50cfc3403a1977af2522
Kristof Dewilde <me@krivi.be>
2016-11-26 02:46:53 +0100

    Limited dummy values to two decimal doubles

--- Raspcast/parser/src/main/java/be/krivi/ucll/da/raspcast/parser/reader/DummyReader.java
+++ Raspcast/parser/src/main/java/be/krivi/ucll/da/raspcast/parser/reader/DummyReader.java
@@ -16,9 +16,17 @@ public class DummyReader implements Reader{
     public WeatherData read(){
 
         WeatherData dummy = new WeatherData();
-        dummy.setTemperature( ThreadLocalRandom.current().nextDouble( -20, 40 + 1 ) );
-        dummy.setHumidity( ThreadLocalRandom.current().nextDouble( 0, 100 + 1 ) );
+        dummy.setTemperature( round( random( -20, 40 ) ) );
+        dummy.setHumidity( round( random( 0, 100 ) ) );
 
         return dummy;
+    }
+
+    private double random( double origin, double bound ){
+        return ThreadLocalRandom.current().nextDouble( origin, bound + 1 );
+    }
+
+    private double round( double value ){
+        return (double)Math.round( value * 100 ) / 100;
     }
 }
